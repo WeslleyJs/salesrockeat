f4869498e22526542d4576cceb1775e10fdc2c4c
Weslley Vieira <84158105+TLT-sptb@users.noreply.github.com>
2022-12-30 11:35:53 -0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- bg-dark bg-gradient -->
-    <header id="header-element" class="p-3 text-white header-controll table">
+    <header id="header-element" class="p-3 text-white header-controll table bg-transi">
         <div class="container header-controll-1">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none ">
@@ -42,6 +42,9 @@ export default {
 }
 </script>
 <style>
+.bg-transi{
+transition: 1s;
+}
 .mImg{
     border-radius: 50%;
 }
