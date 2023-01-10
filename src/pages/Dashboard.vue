4be34955e18b2b3fefa949659ee6eb8aef97f883
Weslley Vieira <84158105+TLT-sptb@users.noreply.github.com>
2023-01-09 21:43:09 -0300

    Add files via upload

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,62 +1,77 @@
 <template>
-        <Header class="bg-dark"/>
-        <div class="transi search-box">
-            <input @keyup.enter="search" type="text" class="search-text" v-model="id" placeholder="MLB">
-            <button type="submit" class="btn search-button" @click="search">
-                <img src="../assets/img/icons8-pesquisar-480.svg" height="35" width="35">
-            </button>
-        </div>
-        <!-- <p>MLB1830777149</p>
-        <p>2742122115</p> -->
-        <!-- http://salehriaz.com/404Page/img/earth.svg -->
-        <img class="object_earth" src="../assets/earth.svg" width="100px">
-        <div v-if="clk">
-            <br>
-            <br>
-            <section v-if="errors">
-                <div v-if="errors" class="alert alert-danger card-group notfound" data-aos="flip-right" data-aos-delay="150"
-                    role="alert">
-                    MLB Invalido ou não existente
-                </div>
-            </section>
-            <section v-else data-aos="flip-right" data-aos-delay="300">
-                <div>
-                    <br>
-                    <div class="myInfo">
-                        <div v-if="mlb" class="card borderT">
-                            <div class="card-body max-li myText">
-                                <nav class="imgPng">
-                                    <img :src="imgPng">
-                                </nav>
-                                <h6><b>Título: {{ mlb.title }}</b></h6>
-                                <p v-if="date"><b>Data de Cadastro: {{ date }}</b></p>
-                                <p><b>Estoque: {{ mlb.available_quantity }}</b></p>
-                                <p><b>{{ mlb.sold_quantity }} Vendidos</b></p>
-                                <p v-if="shipp"><b>Valor Frete Pago Pelo Seller: R${{ shipp }}</b></p>
-                                <p v-if="shipp"><b>Peso de Cadastro: {{ kg }}g</b></p>
-                                <p v-else><b>Envios <span style="font-size:20px; color: green;">FULL</span></b></p>
-                            </div>
-                            <ul class="mx-auto card-group w800">
-                                <li data-aos="flip-right" data-aos-delay="300" class="card-body myBox backgroundEffect"
-                                    v-for="item in mlb.variations" :key="item.id">
-                                    <p><b>Atributos: {{ item.attribute_combinations[0].value_name }}</b></p>
-                                    <p v-if='attribute02'><b>Atributos 02: {{
-                                            item.attribute_combinations[2].value_name
-                                    }}</b></p>
-                                    <p v-else></p>
-                                    <p><b>Estoque: {{ item.available_quantity }}</b></p>
-                                    <p id="myTest"><b>Vendidos: {{ item.sold_quantity }}</b></p>
-                                </li>
-                            </ul>
+    <Header />
+    <div class="mid">
+        <ul>
+            <li id="options" class="ul-btn btn-request" @click="clk = !clk">
+                <h4><b>{{ clk? 'Results': 'DashBoards' }}</b></h4>
+            </li>
+        </ul>
+        <label for="options" @click="clk = !clk"></label>
+    </div>
+    <div class="transi search-box">
+        <input @keyup.enter="search" id="inpt" type="text" class="search-text" v-model="id" placeholder="MLB">
+        <button type="submit" class="btn search-button" @click="search && destroyChart">
+            <img src="../assets/img/icons8-pesquisar-480.svg" height="35" width="35">
+        </button>
+    </div>
+    <p>MLB1830777149</p>
+    <p>2742122115</p>
+    <!-- http://salehriaz.com/404Page/img/earth.svg -->
+    <img class="object_earth" src="../assets/earth.svg" width="100px">
+    <div v-if="clk">
+        <br>
+        <br>
+        <section v-if="errors">
+            <div v-if="errors" class="alert alert-danger card-group notfound" data-aos="flip-right" data-aos-delay="150"
+                role="alert">
+                MLB Invalido ou não existente
+            </div>
+        </section>
+        <section v-else data-aos="flip-right" data-aos-delay="300">
+            <div>
+                <br>
+                <div class="myInfo">
+                    <div v-if="mlb" class="card borderT">
+                        <div class="card-body max-li myText">
+                            <nav class="imgPng">
+                                <img :src="imgPng">
+                            </nav>
+                            <h6><b>Título: {{ mlb.title }}</b></h6>
+                            <p v-if="date"><b>Data de Cadastro: {{ date }}</b></p>
+                            <p><b>Estoque: {{ mlb.available_quantity }}</b></p>
+                            <p><b>{{ mlb.sold_quantity }} Vendidos</b></p>
+                            <p v-if="shipp"><b>Valor Frete Pago Pelo Seller: R${{ shipp }}</b></p>
+                            <p v-if="shipp"><b>Peso de Cadastro: {{ kg }}g</b></p>
+                            <p v-else><b>Envios <span style="font-size:20px; color: green;">FULL</span></b></p>
                         </div>
+                        <ul class="mx-auto card-group w800">
+                            <li data-aos="flip-right" data-aos-delay="300" class="card-body myBox backgroundEffect"
+                                v-for="item in mlb.variations" :key="item.id">
+                                <p><b>Atributos: {{ item.attribute_combinations[0].value_name }}</b></p>
+                                <p v-if='attribute02'><b>Atributos 02: {{
+                                    item.attribute_combinations[2].value_name
+                                }}</b></p>
+                                <p v-else></p>
+                                <p><b>Estoque: {{ item.available_quantity }}</b></p>
+                                <p id="myTest"><b>Vendidos: {{ item.sold_quantity }}</b></p>
+                            </li>
+                        </ul>
+
                     </div>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
+    </div>
+    <div data-aos="flip-right" data-aos-delay="300" class="chart01" :key="id" v-show="!clk && id"
+        style="color:red; height:70vh; width: 80vw; margin-top: 20px;">
+        <canvas data-aos="flip-right" data-aos-delay="300" class="bg-dark bg-gradient" id="myChart"></canvas>
+    </div>
 </template>
 <script>
+import Chart from 'chart.js/auto';
 import axios from 'axios';
-import Header from '../components/Header.vue'
+import Header from '../components/Header.vue';
+const moment = require('moment');
 
 export default {
     name: 'dashboardPage',
@@ -76,8 +91,10 @@ export default {
             loading: '',
             errors: false,
             soldAsc: '',
+            day01: '',
         }
     },
+
     methods: {
         async search() {
             if (!this.id.startsWith('MLB')) this.id = 'MLB' + this.id
@@ -89,7 +106,6 @@ export default {
 
                 const attributes = data.variations;
                 attributes.forEach(element => {
-                    element
                     const imgPng = element.picture_ids;
                     this.imgPng = `http://http2.mlstatic.com/D_${imgPng[0]}-O.jp`
                     const attr = element.attribute_combinations[2];
@@ -100,15 +116,15 @@ export default {
                 const notFound = error.message;
                 this.errors = notFound;
                 setTimeout(() => document.location.reload(true), 1300);
-
             });
             this.shipping();
+            this.visits();
         },
         async shipping() {
             var shp = '';
             var pes = '';
 
-            if(!this.id.startsWith('MLB')) this.id = 'MLB'+this.id
+            if (!this.id.startsWith('MLB')) this.id = 'MLB' + this.id
             await axios.get(`https://api.mercadolibre.com/items/shipping_options/free?ids=${this.id}`).then(res => {
 
                 const data = res.data
@@ -126,23 +142,152 @@ export default {
                 this.errors = err.message;
                 console.log(err.message);
             });
-        }
-    }
+        },
+        async visits() {
+            if (!this.id.startsWith('MLB')) this.id = 'MLB' + this.id;
+            await axios.get(`https://api.mercadolibre.com/items/${this.id}/visits/time_window`, {
+                params: {
+                    last: 7,
+                    unit: 'day',
+                    ending: moment().format('Y-M-D')
+                }
+            }).then((response) => {
+                const datas = response.data.results;
+                var myDatas = datas.map(item => ({
+                    total: item.total,
+                    day: moment(item.date).format('DD-MM'),
+                }
+                ))
+                this.day01 = myDatas
+            }).catch(err => console.warn(err.message));
+
+            this.chart();
+        },
+        async chart() {
+            // rgba(62,0,247,1)
+            const datas = {
+                labels: [
+                    `Dia: ${this.day01[0].day}`,
+                    `Dia: ${this.day01[1].day}`,
+                    `Dia: ${this.day01[2].day}`,
+                    `Dia: ${this.day01[3].day}`,
+                    `Dia: ${this.day01[4].day}`,
+                    `Dia: ${this.day01[5].day}`,
+                    `Dia: ${this.day01[6].day}`,
+                ],
+                datasets: [{
+                    label: 'DashBoard Visits',
+                    data: [
+                        `${this.day01[0].total}`,
+                        `${this.day01[1].total}`,
+                        `${this.day01[2].total}`,
+                        `${this.day01[3].total}`,
+                        `${this.day01[4].total}`,
+                        `${this.day01[5].total}`,
+                        `${this.day01[6].total}`,
+
+                    ],
+                    backgroundColor: [
+                        'rgba(62,0,247,0.5)',
+                        'rgba(62,0,247,0.5)',
+                        'rgba(62,0,247,0.5)',
+                        'rgba(62,0,247,0.5)',
+                        'rgba(62,0,247,0.5)',
+                        'rgba(62,0,247,0.5)',
+                        'rgba(62,0,247,0.5)',
+                    ],
+                    borderColor: [
+                        'rgb(62,0,247)',
+                        'rgb(62,0,247)',
+                        'rgb(62,0,247)',
+                        'rgb(62,0,247)',
+                        'rgb(62,0,247)',
+                        'rgb(62,0,247)',
+                        'rgb(62,0,247)',
+                    ],
+                    borderWidth: 1
+                }],
+
+            }
+            const config = {
+                type: 'bar',
+                data: datas,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            }
+            const ctx = document.getElementById('myChart')
+            const myChart = new Chart(ctx, config)
+            // myChart.render();
+            // myChart.update();
+            myChart;
+        },
+
+    },
 }
 
 </script>
 <style scoped>
-.object_earth{
-    position:absolute;
+
+.mid{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 2%;
+    
+}
+.btn-request{
+    padding: 10px 25px;
+    border: 1px solid #f04725;
+    font-weight: 400;
+    border-radius: 100px;
+    margin-left: 8px;
+    max-width: auto;
+    color: #fff;
+    transition: 1s;
+}
+.btn-request:hover {
+    background: rgb(10, 8, 215);
+    background: radial-gradient(circle, rgba(10, 120, 215, 1) 10%, rgba(10, 8, 215, 1) 100%);
+    color: #fff;
+    transform: scale(1.05);
+    box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.1);
+}
+.object_earth {
+    position: absolute;
     top: 15%;
     width: 40px;
     left: 90%;
     z-index: 90;
-   animation: spin-earth 100s infinite linear both;
+    animation: spin-earth 100s infinite linear both;
 }
-@-moz-keyframes spin-earth { 100% { -moz-transform: rotate(-360deg); transition: transform 20s;  } }
-@-webkit-keyframes spin-earth { 100% { -webkit-transform: rotate(-360deg); transition: transform 20s;  } }
-@keyframes spin-earth{ 100% { -webkit-transform: rotate(-360deg); transform:rotate(-360deg); transition: transform 20s; } }
+
+@-moz-keyframes spin-earth {
+    100% {
+        -moz-transform: rotate(-360deg);
+        transition: transform 20s;
+    }
+}
+
+@-webkit-keyframes spin-earth {
+    100% {
+        -webkit-transform: rotate(-360deg);
+        transition: transform 20s;
+    }
+}
+
+@keyframes spin-earth {
+    100% {
+        -webkit-transform: rotate(-360deg);
+        transform: rotate(-360deg);
+        transition: transform 20s;
+    }
+}
+
 .myInfo {
     display: flex;
     justify-content: center;
@@ -188,7 +333,13 @@ export default {
     align-items: center;
     text-align: center;
     margin-left: 10%;
-    background: #ffffffcd;
+    background: none;
+    color: black;
+    margin-bottom: 5%;
+}
+
+.chart02 {
+    background: #fff
 }
 
 .search-box {
@@ -233,9 +384,10 @@ export default {
     justify-content: center;
     align-items: center;
 }
-.search-button:hover{
-    background: rgb(10,8,215);
-    background: radial-gradient(circle, rgba(10,8,215,1) 10%, rgba(252,75,70,1) 100%);
+
+.search-button:hover {
+    background: rgb(10, 8, 215);
+    background: radial-gradient(circle, rgba(10, 8, 215, 1) 10%, rgba(252, 75, 70, 1) 100%);
     transition: 1s;
 }
 
